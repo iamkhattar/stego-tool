35996fdfad3b73f84bed505df97edea33480544a
Saeed Khan <saeed786129@hotmail.com>
2020-04-19 18:59:05 +0100

    added bin string to bytes conversion method

---
+++ src/model/Write.java
@@ -0,0 +1,24 @@
+package model;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Write {
+
+    /** storing the new image in a new file
+     *
+     * @param path the file path for the new image
+     */
+    public void write(String path, byte[] stegoImage){
+        File f;
+        try{
+            f = new File(path);
+            OutputStream os = new FileOutputStream(f);
+            os.write(stegoImage);
+        }catch(IOException e){
+            System.out.println("Error making new image :" +e);
+        }
+    }
+
+}
